bb934afe517a3da0f0806867de210e3677cbe136
Andrea Griffiths <andreagriffiths11@github.com>
2026-01-24 10:49:26 -0500

    fix: update vite config and file types for deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,13 @@ const defaultTheme = {
   container: {
     center: true,
     padding: "2rem",
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
   },
   extend: {
     screens: {
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
-import { resolve } from 'path'
+import { resolve, dirname } from 'path'
+import { fileURLToPath } from 'url';
 
-const projectRoot = process.env.PROJECT_ROOT || import.meta.dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const projectRoot = process.env.PROJECT_ROOT || __dirname
 
 // https://vite.dev/config/
 export default defineConfig({
