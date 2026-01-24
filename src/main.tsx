bbf0b960ce2a99ca8e019218a1cc3bf76da1d51c
Andrea Griffiths <andreagriffiths11@github.com>
2026-01-24 11:02:52 -0500

    fix: resolve css conflicts and dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -9,5 +9,5 @@ import "./index.css"
 createRoot(document.getElementById('root')!).render(
   <ErrorBoundary FallbackComponent={ErrorFallback}>
     <App />
-   </ErrorBoundary>
+  </ErrorBoundary>
 )
